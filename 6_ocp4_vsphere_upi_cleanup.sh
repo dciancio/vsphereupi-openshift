3feb0beaca9b766ae536be73a97238232823f444
dciancio <dciancio@gmail.com>
2020-02-19 15:49:40 -0500

    fixed comment in 6_ocp4_vsphere_upi_cleanup.sh script

--- 6_ocp4_vsphere_upi_cleanup.sh
+++ 6_ocp4_vsphere_upi_cleanup.sh
@@ -3,6 +3,8 @@
 #DELETE THE BOOTSTRAP'S DNS RECORDS AND INFORM THE INSTALLER TO PROCEED
 #Note, the remaining api and api-int records should continue pointing towards the control-plane servers.
 
+#If you are using a named service for DNS, you can set  BOOTSTRAP_DISABLE_DNS="Y" in 0_ocp4_vsphere_upi_init_vars config file and re-run the 1_ocp4_vsphere_upi_optional_update_dns.sh.  This will generate and push a new DNS config which will disable/remove the bootstrap server from DNS.
+
 source 0_ocp4_vsphere_upi_init_vars
 
 openshift-install --dir $CLUSTER wait-for install-complete
