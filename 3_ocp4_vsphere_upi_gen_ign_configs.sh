b89f5c9c346c65f3cf59e4c58ce5b0fe3af9eb4a
dciancio <dciancio@gmail.com>
2020-02-19 16:41:44 -0500

    fixed line wrap

--- 3_ocp4_vsphere_upi_gen_ign_configs.sh
+++ 3_ocp4_vsphere_upi_gen_ign_configs.sh
@@ -6,8 +6,7 @@ openshift-install --dir $CLUSTER create ignition-configs
 
 systemctl status httpd >/dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "WARNING:  HTTPD service is not running on this system.  The bootstrap.ign file has been generated but will need to manually be copied to the HTTPD serve
-r." >&2
+  echo "WARNING:  HTTPD service is not running on this system.  The bootstrap.ign file has been generated but will need to manually be copied to the HTTPD server." >&2
   exit 1
 else
   [ -f /var/www/html/bootstrap.ign ] && rm -f /var/www/html/bootstrap.ign
